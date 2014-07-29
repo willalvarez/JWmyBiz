f56881f0b555ccdcc27c71f6ddb15fc4cc4d28b9
willalvarez <wcalvarez@icloud.com>
2014-07-28 21:27:46 -0400

    code cleanup 7/28/14

---
+++ myBiz/FBMissingSymbols.h
@@ -0,0 +1,13 @@
+//
+//  FBMissingSymbols.h
+//  myBiz
+//
+//  Created by Will Alvarez on 7/28/14.
+//  Copyright (c) 2014 Will Alvarez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBMissingSymbols : NSObject
+
+@end
---
+++ myBiz/FBMissingSymbols.m
@@ -0,0 +1,13 @@
+//
+//  FBMissingSymbols.m
+//  myBiz
+//
+//  Created by Will Alvarez on 7/28/14.
+//  Copyright (c) 2014 Will Alvarez. All rights reserved.
+//
+
+#import "FBMissingSymbols.h"
+
+@implementation FBMissingSymbols
+
+@end
